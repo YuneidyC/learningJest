774c66f51d0f825ce23780590cbe38d3c042c9f9
YuneidyC <yuneidycastillo25@gmail.com>
2023-05-10 14:10:51 +0100

    Add test with array

--- src/__test__/global.test.js
+++ src/__test__/global.test.js
@@ -1,5 +1,11 @@
 const text = 'Hello World';
+const fruits = ['apple', 'kiwi', 'orange', 'cherry'];
 
 test('Must contain a text', () => {
     expect(text).toMatch(/World/);
 });
+
+test('Do we have a kiwi?', () => {
+    expect(fruits).toContain('kiwi');
+});
+
